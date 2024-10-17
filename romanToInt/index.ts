011a8db4a1856c1c2f8970b05e92dad57c5789db
Julien-FRA <julien.francois@hetic.net>
2024-10-18 01:08:24 +0200

    add kata

---
+++ romanToInt/index.ts
@@ -0,0 +1,27 @@
+const romanToInt = (s: string): number => {
+  let result: number = 0;
+
+  const numbers = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  for (let i = 0; i < s.length; i++) {
+    const current = numbers[s[i]];
+    const next = numbers[s[i + 1]];
+
+    if (current < next) {
+      result += next - current;
+      i++;
+    } else {
+      result += current;
+    }
+  }
+
+  return result;
+};
